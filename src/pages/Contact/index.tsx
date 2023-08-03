aaecba8543559241cd7c505f094cc132ed906b8b
Nithin <nithinkamineni1@gmail.com>
2023-08-03 18:01:57 -0400

    viewer can only send mail through contact us once

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -17,20 +17,26 @@ const contactOpen = "<Contact />";
 const contactDataMail = ContactData.mail1;
 const contactDataPhone = ContactData.phoneNumber;
 
-const defaultOptions = {
-  loop: false,
-  autoplay: true,
-  animationData: lottieData,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice",
-  },
-};
+
 const Contact = () => {
+  
+  const [sentMail, setSentMail] = useState(false);
+
+  const defaultOptions = {
+    loop: !sentMail,
+    autoplay: true,
+    animationData: lottieData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   const [contactData, setContactData] = useState({
     user_name: "",
     user_email: "",
     message: "",
   });
+
   const handleOnchange = (e: any) => {
     setContactData({ ...contactData, [e.target.name]: e.target.value });
   };
@@ -65,12 +71,98 @@ const Contact = () => {
       user_email: "",
       message: "",
     });
+
+    setSentMail(true);
   };
 
-  return (
-    <div className={styles.contact}>
+  if(!sentMail){
+    return (
+      <div className={styles.contact}>
+        <div className={styles.formTagsOpenClose}>
+          <h3 className={styles.contactFormOpen}>{ContactFormOpen}</h3>
+          <motion.div
+            initial='init'
+            animate='anim'
+            exit='last'
+            variants={pageVariants}
+            transition={pageTransition}
+            className={styles.wrapper}
+          >
+            <div className={styles.form}>
+              <h3 className={styles.contactOpen}>{contactOpen}</h3>
+  
+              {/* <h4 className={styles.contactInfo}>{contactInfo}</h4> */}
+              <Button className={styles.contactInfo}
+                size="md"
+                height="48px"
+                width="200px"
+                variant="ghost"
+                color="#DCE2FF"
+                _hover={{ border: '2px solid #1C6FEB' }}
+                leftIcon={<MdPhone color="#1970F1" size="20px" />}>
+                {contactDataPhone}
+              </Button>
+              <Button className={styles.contactInfo}
+                size="md"
+                height="48px"
+                width="200px"
+                variant="ghost"
+                color="#DCE2FF"
+                _hover={{ border: '2px solid #1C6FEB' }}
+                leftIcon={<MdEmail color="#1970F1" size="20px" />}>
+                {contactDataMail}
+              </Button>
+  
+              <form ref={formInputUser} onSubmit={handleOnsubmit}>
+                <input
+                  type='text'
+                  name='user_name'
+                  placeholder='Name'
+                  required
+                  value={contactData.user_name}
+                  onChange={handleOnchange}
+                />
+                <input
+                  type='email'
+                  name='user_email'
+                  placeholder='Email'
+                  required
+                  value={contactData.user_email}
+                  onChange={handleOnchange}
+                />
+                <textarea
+                  name='message'
+                  cols={30}
+                  rows={5}
+                  placeholder='Your Message'
+                  required
+                  value={contactData.message}
+                  onChange={handleOnchange}
+                ></textarea>
+                <button type='submit' value='Send'>Send</button>
+              </form>
+            </div>
+            <div className={styles.lottie}>
+              <Lottie
+                options={defaultOptions}
+                height='100%'
+                width='100%'
+                isStopped={false}
+                isPaused={false}
+              />
+              {/* <Lottie animationData={lottieData} height={'100%'} width={'100%'} autoplay={false} loop={true}/> */}
+            </div>
+          </motion.div>
+          <h3 className={styles.contactFormClose}>{ContactFormClose}</h3>
+        </div>
+      </div>
+    );
+  }
+  else{
+    return (
+      <div className={styles.contact}>
       <div className={styles.formTagsOpenClose}>
-        <h3 className={styles.contactFormOpen}>{ContactFormOpen}</h3>
+      <h3 className={styles.contactFormOpen}>{ContactFormOpen}</h3>
         <motion.div
           initial='init'
           animate='anim'
@@ -79,75 +171,21 @@ const Contact = () => {
           transition={pageTransition}
           className={styles.wrapper}
         >
-          <div className={styles.form}>
-            <h3 className={styles.contactOpen}>{contactOpen}</h3>
-
-            {/* <h4 className={styles.contactInfo}>{contactInfo}</h4> */}
-            <Button className={styles.contactInfo}
-              size="md"
-              height="48px"
-              width="200px"
-              variant="ghost"
-              color="#DCE2FF"
-              _hover={{ border: '2px solid #1C6FEB' }}
-              leftIcon={<MdPhone color="#1970F1" size="20px" />}>
-              {contactDataPhone}
-            </Button>
-            <Button className={styles.contactInfo}
-              size="md"
-              height="48px"
-              width="200px"
-              variant="ghost"
-              color="#DCE2FF"
-              _hover={{ border: '2px solid #1C6FEB' }}
-              leftIcon={<MdEmail color="#1970F1" size="20px" />}>
-              {contactDataMail}
-            </Button>
-
-            <form ref={formInputUser} onSubmit={handleOnsubmit}>
-              <input
-                type='text'
-                name='user_name'
-                placeholder='Name'
-                required
-                value={contactData.user_name}
-                onChange={handleOnchange}
-              />
-              <input
-                type='email'
-                name='user_email'
-                placeholder='Email'
-                required
-                value={contactData.user_email}
-                onChange={handleOnchange}
-              />
-              <textarea
-                name='message'
-                cols={30}
-                rows={5}
-                placeholder='Your Message'
-                required
-                value={contactData.message}
-                onChange={handleOnchange}
-              ></textarea>
-              <button type='submit' value='Send'>Send</button>
-            </form>
-          </div>
           <div className={styles.lottie}>
             <Lottie
               options={defaultOptions}
               height='100%'
               width='100%'
-              isStopped={false}
-              isPaused={false}
+              isStopped={false} // Keep the animation running
+              isPaused={false} // Keep the animation running
             />
-            {/* <Lottie animationData={lottieData} height={'100%'} width={'100%'} autoplay={false} loop={true}/> */}
           </div>
         </motion.div>
         <h3 className={styles.contactFormClose}>{ContactFormClose}</h3>
       </div>
     </div>
-  );
+    );
+  }
 };
 
 export default Contact;
